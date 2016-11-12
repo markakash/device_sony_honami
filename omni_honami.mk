d28cab458cba45f9aebfcf96a097d4ee34d1146a
Sohamlad7 <sohamlad7@gmail.com>
2016-11-12 17:09:57 +0530

    honami: omni bringup
    Change-Id: Id3bbef62d8ecfaf1655511101619b7ed7d7a09d4

---
+++ omni_honami.mk
@@ -0,0 +1,40 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := C6902,C6903,C6906,C6943,C6916,honami
+
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 1080x608
+
+# TWRP Recovery
+DEVICE_RESOLUTION := 1080x1920
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+
+# Kernel source
+TARGET_KERNEL_SOURCE := kernel/sony/msm
+
+# Inherit AOSP Rhine common device parts
+$(call inherit-product, device/sony/honami/aosp_c6903.mk)
+
+# Omni custom config
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit Omni GSM telephony parts
+PRODUCT_PROPERTY_OVERRIDES += telephony.lteOnGSMDevice=1
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Override Product Name for OmniROM
+PRODUCT_NAME := omni_honami
+PRODUCT_MODEL := Xperia Z1
