9b8667f7a5fcafda33f4113787cfdcc63735942e
David <Davidteri91@gmail.com>
2016-11-25 20:15:55 +0100

    honami: call Dalvik/HWUI props for 2GB RAM devices
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -11,6 +11,10 @@ $(call inherit-product, device/sony/rhine-common/platform2.mk)
 # Inherit Lineage common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Dalvik/HWUI
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
 # Fingerprint for honami (from stock)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
