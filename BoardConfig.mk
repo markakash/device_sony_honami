4791a328a3df27027fefbd6caadcf8ff284e45e8
Sohamlad7 <sohamlad7@gmail.com>
2017-04-07 16:35:33 +0530

    honami: Add bootloader name
    Change-Id: I37bc26286569355a7dbe92a5e677dd6e627418da

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,8 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/honami/include
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/honami/bluetooth
 
+TARGET_BOOTLOADER_BOARD_NAME := C6903
+
 # Kernel properties
 TARGET_KERNEL_SOURCE := kernel/sony/msm8974
 TARGET_KERNEL_CONFIG := cm_rhine_honami_defconfig
