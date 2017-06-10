69f9e5e955fedd62cb882e718a0ad158095efba8
Akash <akashniki@gmail.com>
2017-06-10 09:14:18 +0000

    own-ify

--- ownrom.mk
+++ ownrom.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/sony/honami/aosp_c6903.mk)
 $(call inherit-product, device/sony/rhine-common/platform2.mk)
 
 # Inherit Lineage common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
 # Dalvik/HWUI
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
@@ -21,5 +21,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.1.1/14.6.A.
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
 # Override Product Name for LineageOS
-PRODUCT_NAME := lineage_honami
+PRODUCT_NAME := ownrom_honami
 PRODUCT_MODEL := Xperia Z1
